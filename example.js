b1eb2a80f745ca94b8da6dbd1a59d38238b29047
nikitabel0 <146071131+nikitabel0@users.noreply.github.com>
2023-11-28 13:10:08 +0300

    add form.html

---
+++ example.js
@@ -0,0 +1,11 @@
+document.querySelector('')
+
+console.log("adadad")
+console.log(document.querySelector('.theme-button'))
+document.querySelector('.page').classList.remove('light-theme');
+document.querySelector('.page').classList.add('dark');
+let page = document.querySelector('.page');
+let themeButton = document.querySelector('.theme-button');
+themeButton.onclick = function() {
+    console.log('Клик!')
+};
